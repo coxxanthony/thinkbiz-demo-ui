caf0d25c86fafdd8f7b4f30b481244b87c7d5337
Coxlee Anthony Hubilla <coxleehubilla@gmail.com>
2024-02-22 19:53:13 +0800

    menu

---
+++ src/app/page.jsx
@@ -0,0 +1,21 @@
+
+export default function Home() {
+  return (
+		<main className="flex flex-col items-center justify-between p-2">
+			<div className="w-[369px] h-4 relative flex justify-between">
+				<div className="text-slate-600 text-base font-normal font-['Sofia Pro'] uppercase">
+					About
+				</div>
+				<div className="text-slate-600 text-base font-normal font-['Sofia Pro'] uppercase">
+					Service
+				</div>
+				<div className="text-slate-600 text-base font-normal font-['Sofia Pro'] uppercase">
+					Career
+				</div>
+				<div className="text-slate-600 text-base font-normal font-['Sofia Pro'] uppercase">
+					Contact
+				</div>
+			</div>
+		</main>
+  )
+}
